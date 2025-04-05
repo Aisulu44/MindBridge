72c65927bfe05e99f3dc87d11c398e96c27ce2c2
Aisulu44 <119520764+Aisulu44@users.noreply.github.com>
2025-04-05 17:41:21 +0500

    Fix line endings and commit changes

---
+++ MindBridge/base/forms.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+from django.forms import ModelForm
+from .models import *
+
+class DiscussionForm(ModelForm):
+    class Meta:
+        model = Discussion
+        fields = '__all__'
+=======
+from django.forms import ModelForm
+from .models import *
+
+class DiscussionForm(ModelForm):
+    class Meta:
+        model = Discussion
+        fields = '__all__'
+        exclude = ['host', 'participants']
+>>>>>>> master
